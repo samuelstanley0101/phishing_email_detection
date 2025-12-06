fc1bf8204b476db32c4b185972a7f5fbd1ca1b75
Samuel Stanley <samuelstanley0101@gmail.com>
2025-12-05 19:18:14 -0500

    added --examples, --verbose and --silent arguments

--- make-subset.py
+++ make-subset.py
@@ -11,8 +11,19 @@ import pandas as pd
 
 DEFAULT_DATASETS_DIR = "source-datasets"
 kSEED = 0
-
 random.seed(kSEED)
+verbose: bool = False
+silent: bool = False
+
+def print_verbose(*values: object):
+    """
+    Print the given message or object if the verbose flag is set.
+    
+    :param values: The message or object to be printed
+    :type values: object
+    """
+    if verbose:
+        print(*values, flush=True)
 
 def get_files_in_dir(dir: str, recursive: bool = False) -> typing.List[str]:
     """
@@ -33,8 +44,10 @@ def get_files_in_dir(dir: str, recursive: bool = False) -> typing.List[str]:
     for file in dir_files:
         if os.path.isdir(file):
             if recursive:  # add all files in directory if recursive is true
+                print_verbose(f"Found directory {file} in recursive mode, adding all its files to file list.")  # VERBOSE
                 files.extend(get_files_in_dir(file, recursive))
         elif os.path.isfile(file):  # add file to list if file exists
+            print_verbose(f"Found file {file}, adding it to file list.")  # VERBOSE
             files.append(file)
     return files
 
@@ -78,17 +91,26 @@ if __name__ == "__main__":
     subset_size_parser = argparser.add_mutually_exclusive_group(required=True)
     subset_size_parser.add_argument("--examples", "-E", type=int, help="Number of examples in the subset")
     subset_size_parser.add_argument("--percent", "-P", type=float, help="Percent of source dataset(s) to include in subset")
-    argparser.add_argument("-r", action="store_true", dest="recursive", help="Add all files in directories specified recursively")
     argparser.add_argument("--outfile", "-O", type=str, required=True, help="File to write resulting subset to")
+    argparser.add_argument("-r", action="store_true", dest="recursive", help="Add all files in directories specified recursively")
+    verboseness_parser = argparser.add_mutually_exclusive_group(required=False)
+    verboseness_parser.add_argument("--verbose", action="store_true", help="Print additional messages while running")
+    verboseness_parser.add_argument("--silent", action="store_true", help="Print no messages while running")
     argparser.add_argument("files", nargs="*", help="Files and/or directories to use")
     args = argparser.parse_args()
 
-    # temporarily raise exception for --examples argument
-    if args.examples:
-        raise NotImplementedError("Error: --examples argument is not yet implemented")
+    if args.verbose:
+        verbose = True
+    elif args.silent:
+        silent = True
+
+    # # temporarily raise exception for --examples argument
+    # if args.examples:
+    #     raise NotImplementedError("Error: --examples argument is not yet implemented")
 
     # add datasets to args.files if no files supplied
     if len(args.files) == 0:
+        print_verbose(f"No files supplied, using default files from {DEFAULT_DATASETS_DIR} directory.")  # VERBOSE
         datasets = os.listdir(DEFAULT_DATASETS_DIR)
         datasets.remove("Vectorized")  # remove vectorized datasets from list
         for i in range(len(datasets)):
@@ -99,82 +121,93 @@ if __name__ == "__main__":
     files = []
     for file in args.files:
         if os.path.isdir(file):  # add all files in file to list if file is a directory
+            print_verbose(f"Found directory {file}, adding all its files to file list.")  # VERBOSE
             files.extend(get_files_in_dir(file, args.recursive))
         elif os.path.isfile(file):  # add file to list if file exists
+            print_verbose(f"Found file {file}, adding it to file list.")  # VERBOSE
             files.append(file)
     
     # ensure at least one file is in list
     if len(files) == 0:
-        sys.stderr.write("Error: files list must contain at least one real file. Maybe you're having file path issues?\n")
+        sys.stderr.write("Error: Files list must contain at least one real file. Maybe you're having file path issues?\n")
         exit(1)
 
     # get total length of datasets
+    """this could be much more efficient if the csv was parsed manually, but oh well"""
+    print_verbose("Calculating total length of datasets in file list.")  # VERBOSE
     full_dataset_length = 0
     full_dataset_positive_examples = 0
     for file in files:
+        print_verbose(f"Reading file {file} to determine length.")  # VERBOSE
         dataset = pd.read_csv(file, lineterminator='\n')
         full_dataset_length += len(dataset.index)
         full_dataset_positive_examples += num_positive_examples(dataset)
+        print_verbose(f"Found {full_dataset_length} examples in file {file}, {full_dataset_positive_examples} of which were positive.")  # VERBOSE
+
+    # print length
+    if not silent: print(f"Finished reading files for dataset length, found {full_dataset_length} examples.", flush=True)
+    
+    # set proportion
+    if args.examples:  # calculate proportion if the examples argument was given
+        proportion = float(args.examples) / full_dataset_length
+        print_verbose(f"Calculated proportion from desired number of examples as {proportion:.3f}.")  # VERBOSE
+    else:  # args.percent specified, set proportion directly
+        proportion = args.percent
 
     # generate subset
+    if not silent and not verbose:
+        sys.stdout.write("Generating subset")
+        sys.stdout.flush()
+    print_verbose("Generating subset.")  # VERBOSE
+
     subset = pd.DataFrame()
     for file in files:
+        print_verbose(f"Generating subset from file {file}")  # VERBOSE
         dataset = pd.read_csv(file, lineterminator='\n')
         # sample the dataset with stratified (representative) sampling
-        temp_subset = dataset.groupby("label", group_keys=False).sample(frac=args.percent, random_state=kSEED)
+        temp_subset = dataset.groupby("label", group_keys=False).sample(frac=proportion, random_state=kSEED)
+        print_verbose(f"Generated subset from file {file} with {len(temp_subset)} examples, \
+                      {num_positive_examples(temp_subset)} positive and {num_negative_examples(temp_subset)} negative.")  # VERBOSE
         # add the sampled subset to the subset dataframe
         subset = pd.concat([subset, temp_subset[["body", "label"]]], ignore_index=True)
 
-    # # create stratified (representative) splitter object
-    # if args.examples:  # transform number of examples into percent
-    #     subset_percent = float(args.examples) / full_dataset_length
-    # else:
-    #     subset_percent = args.percent
-    #     print(f"subset_percent is {subset_percent}") #DEBUG
-    # sss = StratifiedShuffleSplit(
-    #     n_splits=1,                 # number of splits (subsets) needed
-    #     train_size=subset_percent,  # percent of examples in subset. train_size is a percent if it's a float
-    #     test_size=1,
-    #     random_state=kSEED)         # random seed for consistency
-    
-    # # generate subset
-    # subset = pd.DataFrame()
-    # for file in files:
-    #     dataset = pd.read_csv(file, lineterminator='\n')
-    #     # get training data split
-    #     train_idx, _ = next(sss.split(dataset, dataset["label"]))
-    #     temp_subset = dataset.loc[train_idx].reset_index(drop=True)
-    #     # concatenate body and label columns from temp_subset to subset
-    #     subset = pd.concat([subset, temp_subset[["body", "label"]]], ignore_index=True)
+        # print progress message
+        if not silent and not verbose:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+    if not silent and not verbose: sys.stdout.write("\n"); sys.stdout.flush()
 
-    # # if examples was specified, ensure subset has the correct amount of examples
-    # if args.examples and len(subset.index) < args.examples:  # subset has less examples than required
-    #     num_examples_needed = args.examples - len(subset.index)
-    #     samples_per_file = int(math.ceil(float(num_examples_needed) / len(files)))  # ensure there will be enough samples to cover num_examples_needed
+    # if examples was specified, ensure subset has the correct amount of examples
+    if args.examples and len(subset.index) < args.examples:  # subset has less examples than required
+        print_verbose(f"Resulting subset has less examples than required ({len(subset)} vs {args.examples}). Adding examples.")  # VERBOSE
+        num_examples_needed = args.examples - len(subset.index)
+        samples_per_file = int(math.ceil(float(num_examples_needed) / len(files)))  # ensure there will be enough samples to cover num_examples_needed
 
-    #     # sample datasets for additional examples
-    #     i = 0
-    #     for file in files:
-    #         dataset = pd.read_csv(file, lineterminator='\n')
-    #         j = 0
-    #         while j < samples_per_file and i < num_examples_needed:
-    #             random_row = dataset.sample(n=1, random_state=kSEED)
-    #             subset = pd.concat([subset, random_row[["body", "label"]]], ignore_index=True)  # add random row to subset
-    #             j += 1
-    #             i += 1
-    #         if i >= num_examples_needed:
-    #             break
-    # elif args.examples and len(subset.index) > args.examples:  # subset has more examples than required
-    #     num_excess_examples = len(subset.index) - args.examples
-    #     # Remove num_excess_examples random rows from the subset
-    #     subset = subset.sample(n=args.examples, random_state=kSEED).reset_index(drop=True)
+        # sample datasets for additional examples
+        i = 0
+        for file in files:
+            dataset = pd.read_csv(file, lineterminator='\n')
+            j = 0
+            while j < samples_per_file and i < num_examples_needed:
+                random_row = dataset.sample(n=1, random_state=kSEED)
+                subset = pd.concat([subset, random_row[["body", "label"]]], ignore_index=True)  # add random row to subset
+                j += 1
+                i += 1
+            if i >= num_examples_needed:
+                break
+    elif args.examples and len(subset.index) > args.examples:  # subset has more examples than required
+        print_verbose(f"Resulting subset has more examples than required ({len(subset)} vs {args.examples}). Removing examples.")  # VERBOSE
+        num_excess_examples = len(subset.index) - args.examples
+        # Remove num_excess_examples random rows from the subset
+        subset = subset.sample(n=args.examples, random_state=kSEED).reset_index(drop=True)
 
-    # df = pd.read_csv(files[0])
-    # # get training data split from 
-    # train_idx, _ = next(sss.split(df, df['label']))
-    # subset = df.loc[train_idx].reset_index(drop=True)
-    # print(subset)
-    # print(f"subset has {(percent_positive_examples(subset) * 100):.2f}% positive examples")
+    # print information about subset
+    if not silent:
+        print(f"""Finished generating subset with {len(subset)} examples \
+and a positive proportion of {percent_positive_examples(subset):.3f} \
+from a dataset with {full_dataset_length} examples \
+and a positive proportion of {float(full_dataset_positive_examples) / full_dataset_length:.3f}.""", flush=True)
 
     # write subset to csv without row indicies
+    print_verbose(f"Writing subset to file {args.outfile}.")  # VERBOSE
     subset.to_csv(args.outfile, index=False)
